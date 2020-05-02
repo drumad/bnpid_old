683b892369a9325555d1b04e10c15d70243da11a
amadrazo <andrew.madrazo.contractor@hilton.com>
2020-05-01 18:24:31 -0700

    Added initial SQL tables

---
+++ tables/ClassInfoTable.sql
@@ -0,0 +1,9 @@
+CREATE TABLE class_info
+(
+    `id`        int(11) NOT NULL AUTO_INCREMENT,
+    `classno`   varchar(10)  DEFAULT NULL,
+    `location`  varchar(255) DEFAULT NULL,
+    `begindate` datetime     DEFAULT NULL,
+    `enddate`   datetime     DEFAULT NULL,
+    `classtype` tinyint
+);
